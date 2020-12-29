3588c6d77936e5361788d7bdbe0218aa11219472
Mads Moi-Aune <609372+madsaune@users.noreply.github.com>
2020-12-29 23:21:47 +0100

    Adds comment-based-help

--- Public/Start-TimeOut.ps1
+++ Public/Start-TimeOut.ps1
@@ -1,4 +1,25 @@
 function Start-TimeOut {
+    <#
+    .SYNOPSIS
+        Starts the TimeOut module
+    
+    .DESCRIPTION
+        The Start-TimeOut function starts the timer that sleeps for x minutes (interval) and then displays a notification with a countdown in y seconds (duration).
+    
+    .PARAMETER Interval
+        The interval between each notification.
+    
+    .PARAMETER Duration
+        The duration of the countdown on the notification.
+    
+    .EXAMPLE
+        PS C:\> Start-TimeOut -Interval 15 -Duration 15
+        Starts the TimeOut module with a 15 minutes interval and 15 seconds countdown.
+    
+    .LINK
+        https://github.com/madsaune/TimeOut/blob/main/Public/Start-TimeOut.ps1
+    #>
+
     [CmdletBinding()]
     param (
         [Parameter()]
